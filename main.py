524651185bc69a0d38f8d6b7f8b39d27d439ac31
wacadey <wacadey@gmail.com>
2026-07-22 13:55:36 +0900

    Feat:홈페이지 구성 완료

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI,Request
+# 간단한 홈페이지 제공
+# 1. 모듈 가져오기
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
+# 2. Fastapi 객체 생성, 전역변수 생성
 app = FastAPI()
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
 
+# 3. 라우팅 구성/정의
 @app.get("/")
 def home(req:Request):
-    return templates.TemplateResponse("index.html",{"req":req})
-
+    # 홈페이지("/") get방식 요청 -> 매칭되는 함수 home() 호출됨
+    # -> index.html을 읽어서 req 데이터를 전달하여 동적 html 구성 
+    # -> 응답 (return) -> 클라이언트 브라우저에게 전달 -> 랜더링, Dom tree
+    # -> 브라우저 해석 화면에 그리기 -> 클라이언트는 응답결과를 화면에서 볼수있다
+    return templates.TemplateResponse("index.html", {"request":req})
